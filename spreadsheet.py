fa47bd58bed7becbd7932d3db6ccf5061566fe00
Leo Vuonnala <leo.vuonnala@student.oulu.fi>
2024-10-29 17:27:12 +0200

    user story 1 test 2

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,8 +1,8 @@
+
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
@@ -10,9 +10,10 @@ class SpreadSheet:
     def get(self, cell: str) -> str:
         return self._cells.get(cell, '')
 
-    def evaluate(self, cell: str) -> int | str:
+    def evaluate(self, cell: str) -> str:
         value = self.get(cell)
         if value.isdigit():
             return value
         else:
-            return value
+            return '#Error'
+
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -8,3 +8,8 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1", "1")
         self.assertEqual(spreadsheet.evaluate("A1"), "1")
+
+    def test_evaluate_invalid_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "1.5")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
